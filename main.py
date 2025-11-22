decd5edf3376aba2f34f89a0cbca7571720cec05
RimMirK <github@RimMirK.dev>
2025-11-22 14:59:09 +0100

    debug

--- main.py
+++ main.py
@@ -232,4 +232,7 @@ async def start():
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(start())
+    try:
+        loop.run_until_complete(start())
+    except KeyboardInterrupt:
+        print('except')
