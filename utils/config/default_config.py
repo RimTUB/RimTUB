b6338122b95ff0f0700ca3059e031c0c5494997a
RimMirK <github@rimmirk.dev>
2026-01-06 21:57:14 +0100

    Update version to 2.3-1 in DefaultConfig

--- utils/config/default_config.py
+++ utils/config/default_config.py
@@ -5,8 +5,8 @@ from pyrogram.enums import ClientPlatform
 
 class DefaultConfig:
 
-    VERSION = '2.3'
-    VERSION_TUPLE = (2, 3, 0, 'release', 0)
+    VERSION = '2.3-1'
+    VERSION_TUPLE = (2, 3, 0, 'release', 1)
 
 
     PHONES: List[str]
@@ -59,5 +59,6 @@ class DefaultConfig:
     COMPRESS_OLD_LOGFILES = True
     AUTO_DELETE_OLD_LOGFILES = True
     DELETE_LOGFILES_OLDER_THAN_DAYS = 7
+
 
 
